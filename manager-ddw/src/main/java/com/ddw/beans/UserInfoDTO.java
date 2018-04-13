32295931ae7eafcbd91c3e49dd2ce2fb912f7cdf
q80480305 <80480305@qq.com>
2018-04-13 17:48:40 +0800

    会员

--- manager-ddw/src/main/java/com/ddw/beans/UserInfoDTO.java
+++ manager-ddw/src/main/java/com/ddw/beans/UserInfoDTO.java
@@ -1,45 +1,26 @@
 package com.ddw.beans;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
-import java.util.Date;
-
-@ApiModel(value="用例对像",description="用例对像UserInfoVo")
-public class UserInfoDTO{
-    @ApiModelProperty(name="userName",value="账号（微信、QQ登录时，为空）",example="某某某")
+public class UserInfoDTO {
     private String userName;
-    @ApiModelProperty(name="userPwd",value="密码（微信、QQ登录时，为空）",example="123456")
     private String userPwd;
-    @ApiModelProperty(name="openid",value="用户openid",example="oNSHajg7OZ-K3yqzERRHOzudEm26102")
     private String openid;
-    @ApiModelProperty(name="nickName",value="昵称",example="某某某")
     private String nickName;
-    @ApiModelProperty(name="headImgUrl",value="头像URL",example="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522752012429&di=b26668f45e547cb644bb85d054242abe&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2Fbba1cd11728b4710655829d1c9cec3fdfc0323bc.jpg")
     private String headImgUrl;
-    @ApiModelProperty(name="phone",value="手机号码",example="18500000000")
     private String phone;
-    @ApiModelProperty(name="label",value="标签",example="1,2,3,4")
     private String label;
-    @ApiModelProperty(name="signature",value="个性签名",example="这个人很懒,什么都没有留下...")
     private String signature;
-    @ApiModelProperty(name="sex",value="用户的性别，值为1时是男性，值为2时是女性，值为0时是未知",example="1")
     private Integer sex;
-    @ApiModelProperty(name="registerType",value="注册类型,1 手机注册,2 微信注册,3 QQ注册",example="2")
     private Integer registerType;
-    @ApiModelProperty(name="createTime",value="创建时间",example="2018-04-03 15:58:32")
-    private Date createTime;
-    @ApiModelProperty(name="updateTime",value="更新时间",example="2018-04-03 15:58:32")
-    private Date updateTime;
+    private String createTime;
+    private String updateTime;
 
-    @ApiModelProperty(name="id",value="主键",example="1")
     private Integer id;
 
-    public Date getCreateTime() {
+    public String getCreateTime() {
         return createTime;
     }
 
-    public void setCreateTime(Date createTime) {
+    public void setCreateTime(String createTime) {
         this.createTime = createTime;
     }
 
@@ -115,11 +96,11 @@ public class UserInfoDTO{
         this.signature = signature;
     }
 
-    public Date getUpdateTime() {
+    public String getUpdateTime() {
         return updateTime;
     }
 
-    public void setUpdateTime(Date updateTime) {
+    public void setUpdateTime(String updateTime) {
         this.updateTime = updateTime;
     }
 
--- service-ddw-api/src/main/java/com/ddw/controller/UserController.java
+++ service-ddw-api/src/main/java/com/ddw/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.ddw.controller;
 
-import com.ddw.beans.RequestDTO;
 import com.ddw.beans.ResponseVO;
 import com.ddw.beans.UserInfoDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 访问地址：/swagger-ui.html
@@ -19,21 +21,21 @@ public class UserController {
     @ApiOperation(value = "会员注册用例")
    // @ApiImplicitParam(name = "args", value = "参数", required = true, dataType = "UserInfoDTO")
     @PostMapping("/save")
-    public ResponseVO save(@RequestBody @ApiParam(name="参数",value="传入json格式",required=true)RequestDTO<UserInfoDTO> args){
+    public ResponseVO save(@RequestBody @ApiParam(name="参数",value="传入json格式",required=true)UserInfoDTO args){
         System.out.println(args+"---");
         return new ResponseVO();
     }
 
     @ApiOperation(value = "会员修改资料用例")
     @PostMapping("/update")
-    public ResponseVO update( @RequestBody @ApiParam(name="参数",value="传入json格式",required=true)RequestDTO<UserInfoDTO> args){
+    public ResponseVO update( @RequestBody @ApiParam(name="参数",value="传入json格式",required=true)UserInfoDTO args){
         System.out.println(args+"---");
         return new ResponseVO();
     }
 
     @ApiOperation(value = "会员查询资料用例")
     @PostMapping("/query")
-    public ResponseVO query(@ApiParam(value = "账号", required = true) RequestDTO<UserInfoDTO> args){
+    public ResponseVO query(@ApiParam(value = "账号", required = true) UserInfoDTO args){
         System.out.println(args+"---");
         return new ResponseVO();
     }
