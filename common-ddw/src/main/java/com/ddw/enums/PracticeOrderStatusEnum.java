e7f69e2e4177c38df4297bf694944cacf36961fc
q80480305 <80480305@qq.com>
2018-10-08 11:45:23 +0800

    1、代练退款申请,同个用户多个订单退款申请状态异常bug处理。

--- common-ddw/src/main/java/com/ddw/enums/PracticeOrderStatusEnum.java
+++ common-ddw/src/main/java/com/ddw/enums/PracticeOrderStatusEnum.java
@@ -6,12 +6,16 @@ import com.gen.common.dict.Dictionary;
  * Created by Jacky on 2018/9/10.
  */
 public enum PracticeOrderStatusEnum implements Dictionary {
+    //订单状态，1开始接单，2完成,3代练结算未完成目标（双倍退差价），4用户提前结算1小时内扣违约金（总金额的30%），
+    // 5用户提前结算超过1小时不扣违约金，6退款申请，6退款成功，8退款拒绝
     orderStatus1("开始接单",1),
     orderStatus2("完成",2),
-    orderStatus3("未完成并结单",3),
-    orderStatus4("退款申请",4),
-    orderStatus5("退款成功",5),
-    orderStatus6("退款拒绝",6);
+    orderStatus3("代练结算未完成目标（双倍退差价）",3),
+    orderStatus4("用户提前结算1小时内扣违约金（总金额的30%）",4),
+    orderStatus5("用户提前结算超过1小时不扣违约金",5),
+    orderStatus6("退款申请",6),
+    orderStatus7("退款成功",7),
+    orderStatus8("退款拒绝",8);
 
     private String name;
     private Integer code;
