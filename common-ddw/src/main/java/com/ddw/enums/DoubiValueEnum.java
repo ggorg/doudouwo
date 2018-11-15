cb91646eec8544f756b6beefc11fd1aaaeba043b
deng_84837891@qq.com <Cbpwwksve>
2018-11-16 00:46:13 +0800

    充值卷和逗币价格只能选择，增加约玩历史好友

--- common-ddw/src/main/java/com/ddw/enums/DoubiValueEnum.java
+++ common-ddw/src/main/java/com/ddw/enums/DoubiValueEnum.java
@@ -5,9 +5,20 @@ import com.gen.common.dict.Dictionary;
 /**
  *
  */
-public enum DoubiValueEnum implements Dictionary {
+public enum DoubiValueEnum  {
+    value000("0.1元",100),
+    value00("1元",1000),
+    value0("5元",500),
+    value1("10元",1000),
+    value2("20元",2000),
+    value3("50元",5000),
+    value4("100元",10000),
+    value5("200元",20000),
+    value6("300元",30000),
+    value7("500元",50000),
+    value8("1000元",100000);
     //买进：0，赠送获取：1，已用：2
-    value0("你好美(1.50元)",150),
+    /*value0("你好美(1.50元)",150),
     value1("你很可爱(1.88元)",188),
     value2("你很可爱(1.88元)",188),
     value3("你很大方(5元)",500),
@@ -20,7 +31,7 @@ public enum DoubiValueEnum implements Dictionary {
     value10("我想和你处对像(86.6元)",8660),
     value11("你很迷人(99.9元)",9990),
     value12("想和你认真在一起然后结婚(131.4元)",13140),
-    value13("用命去爱你(199.99元)",19999);
+    value13("用命去爱你(199.99元)",19999);*/
 
 
 
