5689f9784ccf5194db163ad0e4d9dfda04d268d6
deng_84837891@qq.com <Cbpwwksve>
2018-11-28 13:51:24 +0800

    修改获取直播申请状态接口

--- service-ddw-api/src/main/java/com/ddw/beans/LiveReviewStatusVO.java
+++ service-ddw-api/src/main/java/com/ddw/beans/LiveReviewStatusVO.java
@@ -10,17 +10,26 @@ public class LiveReviewStatusVO<T> {
 
 
     @ApiModelProperty(name="liveRadioFlag",value="直播审核标记，0未申请，1审核通过,2审核中,3拒绝",example="直播审核标记，0未申请，1审核通过,2审核中,3拒绝")
-    private Integer liveRadioFlag;
+    private Integer liveRadioFlag=0;
+    @ApiModelProperty(name="goddessFlag",value="女神标记，0非女神，1女神,2审核中,3拒绝",example="1")
+    private Integer goddessFlag=0;
+    @ApiModelProperty(name="realnameFlag",value="实名认证标记，0未实名，1已认证,2审核中,3拒绝",example="1")
+    private Integer realnameFlag=0;
 
-    @ApiModelProperty(name="liveRadioFlagStr",value="",example="")
-    private String liveRadioFlagStr;
-
-    public String getLiveRadioFlagStr() {
-        return liveRadioFlagStr;
+    public Integer getGoddessFlag() {
+        return goddessFlag;
     }
 
-    public void setLiveRadioFlagStr(String liveRadioFlagStr) {
-        this.liveRadioFlagStr = liveRadioFlagStr;
+    public void setGoddessFlag(Integer goddessFlag) {
+        this.goddessFlag = goddessFlag;
+    }
+
+    public Integer getRealnameFlag() {
+        return realnameFlag;
+    }
+
+    public void setRealnameFlag(Integer realnameFlag) {
+        this.realnameFlag = realnameFlag;
     }
 
     public Integer getLiveRadioFlag() {
@@ -29,5 +38,14 @@ public class LiveReviewStatusVO<T> {
 
     public void setLiveRadioFlag(Integer liveRadioFlag) {
         this.liveRadioFlag = liveRadioFlag;
+    }
+
+    @Override
+    public String toString() {
+        return "LiveReviewStatusVO{" +
+                "liveRadioFlag=" + liveRadioFlag +
+                ", goddessFlag=" + goddessFlag +
+                ", realnameFlag=" + realnameFlag +
+                '}';
     }
 }
