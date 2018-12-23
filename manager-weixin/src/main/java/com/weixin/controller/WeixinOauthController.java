853a9ced6ccf7706a457b321ca7180dc02ada3a1
deng_84837891@qq.com <Cbpwwksve>
2018-12-23 14:36:33 +0800

    修改微信授权方法

--- manager-weixin/src/main/java/com/weixin/controller/WeixinOauthController.java
+++ manager-weixin/src/main/java/com/weixin/controller/WeixinOauthController.java
@@ -76,64 +76,66 @@ public class WeixinOauthController {
 
     @RequestMapping(value = "oauth")
     public String oauth(String code, String state, HttpServletRequest request, RedirectAttributes attributes,HttpSession session,Model model) {
-        // 判断用户是否授权
-        logger.info("WeixinOauthController->oauth->code:{},{}",code,state);
-        if (code != null && code.length() > 0) {
-            // 解析经过Base32编码后传递过来的json参数,state长度最长为128
-            //{"appid":"wx695033b74d733b6f","page":"redpack"}  Base32编码 PMRGC4DQNFSCEORCO54DMOJVGAZTGYRXGRSDOMZTMI3GMIRMEJYGCZ3FEI5CE4TFMRYGCY3LEJ6Q
-            JSONObject json = JSONObject.parseObject(new String(new Base32().decode(state)));
-            String appid = json.getString("appid");
-            // 根据传递过来的appid匹配数据库中公众号
-            //Pubweixin pubweixin = pubWeixinService.selectByAppid(json.get("appid").toString());
-            Pubweixin pubweixin = pubWeixinService.selectByAppid(appid);
-            logger.info("pubweixin:"+pubweixin);
-            String openid=null;
-            UserInfo ui = new UserInfo();
-            if (pubweixin != null) {
-                String requestUrl = ACCESS_TOKEN.replace("APPID", pubweixin.getAppid()).replace("SECRET", pubweixin.getAppsecret()).replace("CODE", code);
 
-                // 通过code换取网页授权access_token
-                logger.info("WeixinOauthController->oauth->请求地址->{}",requestUrl);
+        try {
+            // 判断用户是否授权
+            logger.info("WeixinOauthController->oauth->code:{},{}",code,state);
+            if (code != null && code.length() > 0 && StringUtils.isNotBlank(state)) {
+                // 解析经过Base32编码后传递过来的json参数,state长度最长为128
+                //{"appid":"wx695033b74d733b6f","page":"redpack"}  Base32编码 PMRGC4DQNFSCEORCO54DMOJVGAZTGYRXGRSDOMZTMI3GMIRMEJYGCZ3FEI5CE4TFMRYGCY3LEJ6Q
+                JSONObject json = JSONObject.parseObject(new String(new Base32().decode(state)));
+                String appid = json.getString("appid");
+                // 根据传递过来的appid匹配数据库中公众号
+                //Pubweixin pubweixin = pubWeixinService.selectByAppid(json.get("appid").toString());
+                Pubweixin pubweixin = pubWeixinService.selectByAppid(appid);
+                logger.info("pubweixin:"+pubweixin);
+                String openid=null;
+                UserInfo ui = new UserInfo();
+                if (pubweixin != null) {
+                    String requestUrl = ACCESS_TOKEN.replace("APPID", pubweixin.getAppid()).replace("SECRET", pubweixin.getAppsecret()).replace("CODE", code);
 
-                JSONObject jsonObject = WeixinUtil.httpRequest(requestUrl, "GET", null);
-                logger.info("WeixinOauthController->oauth->响应->{}",jsonObject);
-                // 如果授权则继续，没有授权则直接返回
-                if (jsonObject != null && jsonObject.containsKey("openid")) {
-                    /** 关注状态，0未关注，1已关注，-1数据库没数据 */
-                    int subscribe = -1;
+                    // 通过code换取网页授权access_token
+                    logger.info("WeixinOauthController->oauth->请求地址->{}",requestUrl);
+
+                    JSONObject jsonObject = WeixinUtil.httpRequest(requestUrl, "GET", null);
+                    logger.info("WeixinOauthController->oauth->响应->{}",jsonObject);
+                    // 如果授权则继续，没有授权则直接返回
+                    if (jsonObject != null && jsonObject.containsKey("openid")) {
+                        /** 关注状态，0未关注，1已关注，-1数据库没数据 */
+                        int subscribe = -1;
                     /*用户openid */
-                    openid = jsonObject.get("openid").toString();
-                    ui = weixinUserService.selectByopenid(openid);
-                    if (ui == null) {
-                        subscribe = -1;
-                    } else {
-                        subscribe = ui.getSubscribe().equals("关注") ? 1 : 0;
-                    }
+                        openid = jsonObject.get("openid").toString();
+                        ui = weixinUserService.selectByopenid(openid);
+                        if (ui == null) {
+                            subscribe = -1;
+                        } else {
+                            subscribe = ui.getSubscribe().equals("关注") ? 1 : 0;
+                        }
 
-                    // 拥有获取用户信息权限
-                    if (subscribe == -1 && jsonObject.get("scope").toString().contains("snsapi_userinfo")) {
-                        // TODO 校验access_token是否有效，无效调用刷新获取access_token（非必须）
-                        JSONObject userinfo = this.userinfo(jsonObject.get("access_token").toString(), openid);
-                        ui = weixinUtil.parseJsonToUserInfo(userinfo, openid, pubweixin.getAppid());
-                        ui.setSubscribe("0");
-                        // 用户信息入库
-                        weixinUserService.save(ui);
+                        // 拥有获取用户信息权限
+                        if (subscribe == -1 && jsonObject.get("scope").toString().contains("snsapi_userinfo")) {
+                            // TODO 校验access_token是否有效，无效调用刷新获取access_token（非必须）
+                            JSONObject userinfo = this.userinfo(jsonObject.get("access_token").toString(), openid);
+                            ui = weixinUtil.parseJsonToUserInfo(userinfo, openid, pubweixin.getAppid());
+                            ui.setSubscribe("0");
+                            // 用户信息入库
+                            weixinUserService.save(ui);
+                        }
                     }
                 }
-            }
-            //TODO 这里根据state传递过来的参数page,跳转到对应页面
-            if(json.containsKey("page")){
-                String page = json.getString("page");
-                String urlAppendParam=null;
-                if("redpack".equals(page)){
-                    return InternalResourceViewResolver.REDIRECT_URL_PREFIX + "pages/manager/weixin/menu";
-                }
-                if("obn".equals(page)){
-                    // 这里绑定老带新,然后跳转到下载APP页面
-                    if(json.containsKey("param")){
-                        Map oldUser = oldBringingNewService.getOpenid(json.get("param").toString());
-                        if(oldUser != null){
-                            try {
+                //TODO 这里根据state传递过来的参数page,跳转到对应页面
+                if(json.containsKey("page")){
+                    String page = json.getString("page");
+                    String urlAppendParam=null;
+                    if("redpack".equals(page)){
+                        return InternalResourceViewResolver.REDIRECT_URL_PREFIX + "pages/manager/weixin/menu";
+                    }
+                    if("obn".equals(page)){
+                        // 这里绑定老带新,然后跳转到下载APP页面
+                        if(json.containsKey("param")){
+                            Map oldUser = oldBringingNewService.getOpenid(json.get("param").toString());
+                            if(oldUser != null){
+
                                 if (userInfoService.countUser(ui.getUnionid())==0){
                                     String oldOpenid = oldUser.get("openid").toString();
                                     String headImgUrl = oldUser.get("headImgUrl").toString();
@@ -158,80 +160,77 @@ public class WeixinOauthController {
 
                                     urlAppendParam=Base64Utils.encodeToString(JSONObject.toJSONString(map).getBytes());
                                 }
-                            } catch (Exception e) {
-                                logger.error("WeixinOauthController->oauth->老带新注册新用户",e);
+
                             }
                         }
-                    }
-                }else if("shop".equals(page)){
-                    logger.info("h5商城{}",page);
-                    try {
-                        String shopTokenStr=Tools.getCookie("shopToken");
-                        if(StringUtils.isBlank(ui.getUnionid()) && StringUtils.isNotBlank(shopTokenStr)){
+                    }else if("shop".equals(page)){
+                        logger.info("h5商城{}",page);
+
+                            String shopTokenStr=Tools.getCookie("shopToken");
+                            if(StringUtils.isBlank(ui.getUnionid()) && StringUtils.isNotBlank(shopTokenStr)){
+                                return WXGlobals.getOauthJumUrlByKey(page);
+                            }else if(StringUtils.isBlank(ui.getUnionid()) && StringUtils.isBlank(shopTokenStr)){
+                                return "/404";
+                            }
+
+                            Map userMap=userInfoService.getUser(ui.getUnionid());
+                            Integer userId=null;
+                            if (userMap==null || userMap.isEmpty() || userMap.size()==0){
+                                //注册账号
+                                UserInfoDTO userInfoDTO = new UserInfoDTO();
+                                userInfoDTO.setHeadImgUrl(ui.getHeadimgurl());
+                                userInfoDTO.setNickName(ui.getNickname());
+                                userInfoDTO.setOpenid(ui.getUnionid());
+                                userInfoDTO.setRealOpenid(openid);
+                                userInfoDTO.setUnionID(ui.getUnionid());
+                                userInfoDTO.setUserName(ui.getNickname());
+                                userInfoDTO.setSex(Integer.valueOf(ui.getSex()));
+                                userInfoDTO.setRegisterType(1);
+                                ResponseVO<Integer> res= userInfoService.save(userInfoDTO);
+                                userId=res.getData();
+                            }else{
+                                userId=(Integer) userMap.get("id");
+                            }
+
+                            String[] params=json.getString("param").split("_");
+                            Map cookieM=new HashMap();
+                            String base64Token=BaseTokenUtil.createToken(ui.getUnionid());
+                            cookieM.put("tableNumber",params[1]);
+                            cookieM.put("t",base64Token);
+                            BaseTokenUtil.putUserIdAndStoreId(base64Token,userId,Integer.parseInt(params[0]),openid);
+                            Tools.setCookie("shopToken",URLEncoder.encode(TydicDES.encodeValue(JSONObject.toJSONString(cookieM)),"utf-8"));
                             return WXGlobals.getOauthJumUrlByKey(page);
-                        }else if(StringUtils.isBlank(ui.getUnionid()) && StringUtils.isBlank(shopTokenStr)){
-                            return "/404";
-                        }
-
-                        Map userMap=userInfoService.getUser(ui.getUnionid());
-                        Integer userId=null;
-                        if (userMap==null || userMap.isEmpty() || userMap.size()==0){
-                            //注册账号
-                            UserInfoDTO userInfoDTO = new UserInfoDTO();
-                            userInfoDTO.setHeadImgUrl(ui.getHeadimgurl());
-                            userInfoDTO.setNickName(ui.getNickname());
-                            userInfoDTO.setOpenid(ui.getUnionid());
-                            userInfoDTO.setRealOpenid(openid);
-                            userInfoDTO.setUnionID(ui.getUnionid());
-                            userInfoDTO.setUserName(ui.getNickname());
-                            userInfoDTO.setSex(Integer.valueOf(ui.getSex()));
-                            userInfoDTO.setRegisterType(1);
-                           ResponseVO<Integer> res= userInfoService.save(userInfoDTO);
-                            userId=res.getData();
-                        }else{
-                            userId=(Integer) userMap.get("id");
-                        }
-
-                        String[] params=json.getString("param").split("_");
-                        Map cookieM=new HashMap();
-                        String base64Token=BaseTokenUtil.createToken(ui.getUnionid());
-                        cookieM.put("tableNumber",params[1]);
-                        cookieM.put("t",base64Token);
-                        BaseTokenUtil.putUserIdAndStoreId(base64Token,userId,Integer.parseInt(params[0]),openid);
-                        Tools.setCookie("shopToken",URLEncoder.encode(TydicDES.encodeValue(JSONObject.toJSONString(cookieM)),"utf-8"));
-                       return WXGlobals.getOauthJumUrlByKey(page);
-                    }catch (Exception e){
-                        logger.error("WeixinOauthController->oauth->h5商城跳转失败",e);
 
                     }
-                }
-                String jumpUrlValue= WXGlobals.getOauthJumUrlByKey(page);
-                if(StringUtils.isNotBlank(jumpUrlValue)){
+                    String jumpUrlValue= WXGlobals.getOauthJumUrlByKey(page);
+                    if(StringUtils.isNotBlank(jumpUrlValue)){
 
-                   // AccessToken at=weixinInterfaceService.getTokenByAppid(appid);
-                    logger.info("WeixinOauthController->oauth->配置文件url->url:{}",jumpUrlValue);
-                   // WeiXinTools.initTicket(at.getTicket(),at.getAppid());
-                    logger.info("WeixinOauthController->oauth->跳转->page:{},openid:{}",page,openid);
-                    StringBuilder url=new StringBuilder();
-                    url.append(InternalResourceViewResolver.REDIRECT_URL_PREFIX );
-                    url.append(jumpUrlValue);
-                    if(urlAppendParam!=null){
-                        url.append("?param="+urlAppendParam);
-                    }
-                    //url.append("?token=");
-                   // url.append(MyEncryptUtil.encry(StringUtils.isBlank(this.WXGlobals.getTestOpenid())?openid:this.WXGlobals.getTestOpenid()));
+                        // AccessToken at=weixinInterfaceService.getTokenByAppid(appid);
+                        logger.info("WeixinOauthController->oauth->配置文件url->url:{}",jumpUrlValue);
+                        // WeiXinTools.initTicket(at.getTicket(),at.getAppid());
+                        logger.info("WeixinOauthController->oauth->跳转->page:{},openid:{}",page,openid);
+                        StringBuilder url=new StringBuilder();
+                        url.append(InternalResourceViewResolver.REDIRECT_URL_PREFIX );
+                        url.append(jumpUrlValue);
+                        if(urlAppendParam!=null){
+                            url.append("?param="+urlAppendParam);
+                        }
+                        //url.append("?token=");
+                        // url.append(MyEncryptUtil.encry(StringUtils.isBlank(this.WXGlobals.getTestOpenid())?openid:this.WXGlobals.getTestOpenid()));
                    /*if(json.containsKey("param") && json.getString("param")!=null){
                        url.append("&").append("param=").append(json.getString("param"));
                    }*/
-                    logger.info("WeixinOauthController->oauth->跳转->url:{}",url.toString());
-                   return url.toString();
+                        logger.info("WeixinOauthController->oauth->跳转->url:{}",url.toString());
+                        return url.toString();
 
+                    }
                 }
             }
+        }catch (Exception e){
+            logger.error("WeixinOauthController->oauth->跳转失败",e);
         }
-
         // 所有请求都跳转到此页面上
-        return InternalResourceViewResolver.REDIRECT_URL_PREFIX + "/html/main.html";
+        return InternalResourceViewResolver.REDIRECT_URL_PREFIX + "/404";
     }
 
     /**
